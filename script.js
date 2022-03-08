5f878fa1c051980f60769cf6ef99ba147363737b
White Pegasus <whitepegasus1603@gmail.com>
2022-03-08 23:38:56 +0530

    third commit

--- script.js
+++ script.js
@@ -15,21 +15,26 @@ const submitBtn=document.getElementById("submit");
 const skillSpeciality=document.getElementById("speciality");
 const skillWk=document.getElementById("skill-wk");
 const playerImage = document.getElementById("player-image");
+const leftColumn=document.getElementById("left-column");
+const rightColumn=document.getElementById("right-column");
+const soldDiv=document.getElementById("sold-div");
+const footer=document.getElementById("footer");
+const soldDetail=document.getElementById("sold-detail");
+const gotoNextBidBtn= document.getElementById("goto-next-bid");
 
+const teamNames=["RR","CSK","KKR","DC","RCB","SRH"];
+const maxPoint=1700; //maximum bid value
 
 //fetching data from sheet1
 let playerJsonData;
 let sNo;
-const getData = async () => {
-    try {
-      const res = await fetch(
-        //"https://sheet.best/api/sheets/0f7a6adf-3eab-4c4f-bf25-9fd9d7adabd0"  //sheet link (from sheet.best)
-        "https://sheet.best/api/sheets/49f53ffd-474d-4f7b-9538-06519c0175f2"
+const getData = async () => {   // input sheet: https://docs.google.com/spreadsheets/d/1YbGjVNyBVm14jz-FsrWaziq7EuRvUoX8a8PFuGkaLJo/edit?usp=sharing
+    try { 
+      const res = await fetch( //sheet link (from sheet.best)
+        "https://sheet.best/api/sheets/4af5b600-38ab-40df-8f16-0e1a652b3f86"
       );
       playerJsonData = await res.json();
       console.log(playerJsonData); // player data
-      console.log("size:"+playerJsonData.length);
-      //setData(Object.keys(data).map((key) => data[key]));
     } catch (error) {
       console.log(error);
     }
@@ -70,27 +75,32 @@ function randomPlayer(){
     else{
       skillWk.style.display="block";
     }
+}
 
+function displayNextPlayer(){
+  if(confirm("Start Bidding ?")==true){
+    startBtn.innerText=`Next Player`;
+    bidValue.innerHTML="0000";
+    bidderName.innerHTML="Unsold";
+    newBidValue.value="";
+    newBidderName.value="select";
+    playerImage.src="default-image.png";
+    randomPlayer(); 
+  }
 }
 
 //Selecting random player from the list
-startBtn.addEventListener("click",()=>{
-    if(confirm("Start Bidding ?")==true){
-      startBtn.innerText=`Next Player`;
-      bidValue.innerHTML="0000";
-      bidderName.innerHTML="Unsold";
-      newBidValue.value="";
-      newBidderName.value="select";
-      randomPlayer(); 
-    }
-
-});
+startBtn.addEventListener("click",displayNextPlayer);
 
 
 //Updating player bid values
+let bidderIndex;
 bidButton.addEventListener("click",()=>{
     const val=newBidValue.value;
-    const bidder=newBidderName.value;
+    const currIdx=parseInt(newBidderName.value);
+    const bidder=teamNames[currIdx];
+    //const bidder=newBidderName.value;
+    
 
     if(val==''){
         alert('Bid Value Field Empty!')
@@ -101,7 +111,11 @@ bidButton.addEventListener("click",()=>{
     else if(parseInt(val)<parseInt(bidValue.innerText)){
       alert("Current bid value less than previous bid!");
     }
+    else if(parseInt(teamPointsJson[currIdx].total)+parseInt(val) > maxPoint){
+      alert("Not enough point!")
+    }
     else{
+        bidderIndex=parseInt(newBidderName.value);
         bidValue.innerHTML=val;
         bidderName.innerHTML=bidder;
         newBidValue.value="";
@@ -126,65 +140,8 @@ submitBtn.addEventListener("click",()=>{
   if(confirm("Are you sure you want to submit?")==true){
     handleSubmit();
   }
-  
 });
 
-
-//Uploaded Excel data to JSon data 
-/*
-//html
-/*
-<body class="w3-light-grey">
-
-  <div class="conatiner mt-5">
-    <div class="row">
-        <div class="col-md-3"></div>
-        <div class="col-md-3">
-            <input class="form-control" type="file" id="input" accept=".xls,.xlsx"  >
-        </div>
-        <div class="col-md-2">
-            <button class="btn btn-primary" id="button">Convert</button>
-        </div>
-<div class="col-md-12">
-<pre id="jsondata"></pre>
-</div>
-    </div>
-</div>
-</body>
-
-
-
-let selectedFile;
-//console.log(window.XLSX);
-document.getElementById('input').addEventListener("change", (event) => {
-    selectedFile = event.target.files[0];
-})
-
-let data=[{
-    "name":"jayanth",
-    "data":"scd",
-    "abc":"sdef"
-}];
-
-
-document.getElementById('button').addEventListener("click", () => {
-    XLSX.utils.json_to_sheet(data, 'out.xlsx');
-    if(selectedFile){
-        let fileReader = new FileReader();
-        fileReader.readAsBinaryString(selectedFile);
-        fileReader.onload = (event)=>{
-         let data = event.target.result;
-         let workbook = XLSX.read(data,{type:"binary"});
-         //console.log(workbook);
-         workbook.SheetNames.forEach(sheet => {
-              playerJsonData = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet]);
-              console.log(playerJsonData);
-              document.getElementById("jsondata").innerHTML = JSON.stringify(playerJsonData,undefined,4)
-         });
-        }
-    }
-});
-*/
 
 //Function to Add data of sold player to new sheet
 const data={
@@ -208,7 +165,7 @@ const addData=()=>{
     .then((data) => {
     // The response comes here
     console.log(data);
-      console.log("Added data to https://sheet.best/api/sheets/4466ce3f-5b1b-4202-9eef-3d2c53f0b7cb" );
+      console.log("Added data to https://docs.google.com/spreadsheets/d/1TiyZuo8o12M2LU35P9ZzO2-LWx5C91PJ_LMdch_N8EA/edit#gid=1278357720" );
     })
     .catch((error) => {
     // Errors are reported there
@@ -217,14 +174,14 @@ const addData=()=>{
 }
 
   //Function to delete row from sheet
-  const deleteRow=()=>{
-    fetch(`https://sheet.best/api/sheets/49f53ffd-474d-4f7b-9538-06519c0175f2/${sNo}`, {  //change last didgit to manipulate the row you want to delete
+  const deleteRow=()=>{  //input sheet: https://docs.google.com/spreadsheets/d/1YbGjVNyBVm14jz-FsrWaziq7EuRvUoX8a8PFuGkaLJo/edit?usp=sharing
+    fetch(`https://sheet.best/api/sheets/4af5b600-38ab-40df-8f16-0e1a652b3f86/${sNo}`, {  //change last didgit to manipulate the row you want to delete
         method: "DELETE",
       })
         .then((response) => response.json())
         .then((data) => {
           console.log(data);
-          console.log("Deleted from https://sheet.best/api/sheets/0f7a6adf-3eab-4c4f-bf25-9fd9d7adabd0");
+          console.log("Deleted from https://docs.google.com/spreadsheets/d/1YbGjVNyBVm14jz-FsrWaziq7EuRvUoX8a8PFuGkaLJo/edit?usp=sharing");
         })
         .catch((error) => {
           console.error(error);
@@ -263,6 +220,10 @@ const handleSubmit = async (e) => {
       data.Name=currentPlayerName;
       data.Dept=currentPlayerDept;
       data.Year=currentPlayerYear;
+    if(currentPlayerName==""){
+      alert("Bidding is not started yet");
+    }
+    else{
 
     if(bidValue.innerText==="0000")
     {
@@ -275,11 +236,93 @@ const handleSubmit = async (e) => {
     else{
       data.Team=bidderName.innerText;
       data.Points=bidValue.innerText;
-      addData();
-      deleteRow();
+      //addData();  uncomment it
+      //deleteRow(); uncomment it
+      const newPoints=parseInt(data.Points)+parseInt(teamPointArr[bidderIndex]);
+      updateTeamPointSheet(data.Team,newPoints);          //update points sheet
     }
+
+    leftColumn.style.display="none";
+    rightColumn.style.display="none";
+
+    soldDiv.style.display="block";
+    footer.classList.add("footer-align");
+
+    if(bidValue.innerText=="0000"){
+      soldDetail.innerHTML=`${currentPlayerName} unsold`;
+    }
+    else{
+      soldDetail.innerHTML=`${currentPlayerName} sold to ${bidderName.innerText} for ${bidValue.innerText} points`;
+    }
+    }
+
   }
 
+  gotoNextBidBtn.addEventListener("click",()=>{
+    leftColumn.style.display="block";
+    rightColumn.style.display="block";
+
+    soldDiv.style.display="none";
+    footer.classList.remove("footer-align");
+    displayNextPlayer();
+  });
+
+  /* 
+    0 = rr
+    1 = csk
+    2 = kkr
+    3 = dc
+    4 = rcb
+    5 = srh
+  */
+
+
+  //fetching points from point sheet
+let teamPointsJson;
+let teamPointArr=[];
+const getCurrentPoints = async () => {
+    try {
+      const res = await fetch( //sheet link (from sheet.best)   // team point sheet: https://sheet.best/api/sheets/1d7eb585-89cf-4925-b18a-2681b62e70d1
+        "https://sheet.best/api/sheets/1d7eb585-89cf-4925-b18a-2681b62e70d1"
+      );
+      teamPointsJson = await res.json();
+      console.log(teamPointsJson); // player data
+      console.log("size:"+teamPointsJson.length);
+
+      teamPointArr.push(teamPointsJson[0].total);
+      teamPointArr.push(teamPointsJson[1].total);
+      teamPointArr.push(teamPointsJson[2].total);
+      teamPointArr.push(teamPointsJson[3].total);
+      teamPointArr.push(teamPointsJson[4].total);
+      teamPointArr.push(teamPointsJson[5].total);
+
+      console.log(teamPointArr);
+      //setData(Object.keys(data).map((key) => data[key]));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  getCurrentPoints();
+
+
+  const updateTeamPointSheet=(teamName,newPoint)=>{
+    fetch(
+      `https://sheet.best/api/sheets/1d7eb585-89cf-4925-b18a-2681b62e70d1/team/*${teamName}*`,
+      {
+        method: "PATCH",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          total: `${newPoint}`,
+        }),
+      }
+    )
+      .then((r) => r.json())
+      .then(console.log)
+      .catch(console.error);
+}
 
   
 
